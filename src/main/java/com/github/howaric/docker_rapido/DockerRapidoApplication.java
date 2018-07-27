1e05093d7b9cd596627f62722128564a0de7e717
ezquaha <hao.quan@ericsson.com>
2018-07-27 13:48:26 +0800

    Add banner for docker-rapido

--- src/main/java/com/github/howaric/docker_rapido/DockerRapidoApplication.java
+++ src/main/java/com/github/howaric/docker_rapido/DockerRapidoApplication.java
@@ -1,14 +1,8 @@
 package com.github.howaric.docker_rapido;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -21,53 +15,35 @@ import com.github.howaric.docker_rapido.utils.RapidoLogCentre;
 @SpringBootApplication
 public class DockerRapidoApplication {
 
-	private static final String logDir = "log.dir";
-	private static final String DOCKER_RAPIDO = "docker-rapido";
+    private static final String logDir = "log.dir";
+    private static final String DOCKER_RAPIDO = "docker-rapido";
 
-	public static void main(String[] args) {
-		// RapidoLogCentre.printLinsInBox(getBanner());
-		printBanner();
-		CliOptions cliOptions = new CliOptions();
-		JCommander jcommander = JCommander.newBuilder().addObject(cliOptions).build();
-		jcommander.parse(args);
-		jcommander.setProgramName(DOCKER_RAPIDO);
-		if (cliOptions.isHelp() || cliOptions.needShowUsage()) {
-			jcommander.usage();
-			return;
-		}
-		setLogDir(cliOptions);
-		Logger logger = LoggerFactory.getLogger(DockerRapidoApplication.class);
-		RapidoLogCentre.printLinsInBox(getBanner());
-		logger.info("Get cli params:\n\n{}\n", CommonUtil.prettyJson(cliOptions));
-		if (cliOptions.isWebMode()) {
-			SpringApplication.run(DockerRapidoApplication.class, args);
-		} else {
-			RapidoCliApplication.run(cliOptions);
-		}
-	}
+    public static void main(String[] args) {
+        printBanner();
+        CliOptions cliOptions = new CliOptions();
+        JCommander jcommander = JCommander.newBuilder().addObject(cliOptions).build();
+        jcommander.parse(args);
+        jcommander.setProgramName(DOCKER_RAPIDO);
+        if (cliOptions.isHelp() || cliOptions.needShowUsage()) {
+            jcommander.usage();
+            return;
+        }
+        setLogDir(cliOptions);
+        RapidoLogCentre.printLinsInBox(Arrays.asList("IsDeclaredOfficial: " + cliOptions.isDeclareOfficial(),
+                "TemplateFile: " + cliOptions.getTemplateFilePath(), "ToBuildImageTag: " + cliOptions.getImageTag()));
+        if (cliOptions.isWebMode()) {
+            SpringApplication.run(DockerRapidoApplication.class, args);
+        } else {
+            RapidoCliApplication.run(cliOptions);
+        }
+    }
 
-	private static void printBanner() {
-		File bannerFile = CommonUtil.readTemplateFile("classpath:banner.txt");
-		try {
-			System.out.println(FileUtils.readFileToString(bannerFile));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private static void printBanner() {
+        String result = CommonUtil.readResourcesFileToString("banner.txt");
+        System.out.println(result);
+    }
 
-	private static List<String> getBanner() {
-		Arrays.asList("Welcome to use docker-rapido!");
-		List<String> result = new ArrayList<>();
-		File bannerFile = CommonUtil.readResourcesFile("banner.txt");
-		try {
-			result = FileUtils.readLines(bannerFile);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-
-	private static void setLogDir(CliOptions cliOptions) {
-		System.setProperty(logDir, cliOptions.getLogDir() + File.separator);
-	}
+    private static void setLogDir(CliOptions cliOptions) {
+        System.setProperty(logDir, cliOptions.getLogDir() + File.separator);
+    }
 }
--- src/main/java/com/github/howaric/docker_rapido/utils/CommonUtil.java
+++ src/main/java/com/github/howaric/docker_rapido/utils/CommonUtil.java
@@ -1,12 +1,17 @@
 package com.github.howaric.docker_rapido.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -18,58 +23,114 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CommonUtil {
 
-	private static Logger logger = LoggerFactory.getLogger(CommonUtil.class);
+    private static Logger logger = LoggerFactory.getLogger(CommonUtil.class);
 
-	public static File readTemplateFile(String path) {
-		try {
-			File file = ResourceUtils.getFile(path);
-			return file;
-		} catch (Exception e) {
-			logger.error("File doesn't exist: {}", path);
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public static File readTemplateFile(String path) {
+        try {
+            File file = ResourceUtils.getFile(path);
+            return file;
+        } catch (Exception e) {
+            logger.error("File doesn't exist: {}", path);
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	public static File readResourcesFile(String path) {
-		return null;
-	}
+    public static List<String> readResourcesFileToList(String resourcesFile) {
+        List<String> result = new ArrayList<>();
+        String line = "";
+        InputStream inputStream = CommonUtil.getResourcesFileInputStream(resourcesFile);
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                result.add(line);
+            }
+        } catch (Exception e) {
+            try {
+                bufferedReader.close();
+                inputStreamReader.close();
+                inputStream.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+        return result;
+    }
+    
+    public static String readResourcesFileToString(String resourcesFile) {
+        StringBuilder result = new StringBuilder();
+        String line = "";
+        InputStream inputStream = CommonUtil.getResourcesFileInputStream(resourcesFile);
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                result.append(line);
+                result.append("\n");
+            }
+        } catch (Exception e) {
+            try {
+                bufferedReader.close();
+                inputStreamReader.close();
+                inputStream.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+        return result.toString();
+    }
 
-	public static void sleep(int time) {
-		try {
-			Thread.sleep(time);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public static InputStream getResourcesFileInputStream(String resourcesRelativePath) {
+        String jarPath = CommonUtil.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        InputStream inputStream = null;
+        try {
+            URL url = new URL("jar:" + jarPath + resourcesRelativePath);
+            inputStream = url.openStream();
+        } catch (MalformedURLException e) {
+            inputStream = CommonUtil.class.getClass().getResourceAsStream("/" + resourcesRelativePath);
+        } catch (IOException e) {
+            logger.error("Resource file {} get stream failed", resourcesRelativePath);
+            e.printStackTrace();
+        }
+        return inputStream;
+    }
 
-	public static String prettyJson(Object bean) {
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-			return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(bean);
-		} catch (JsonProcessingException e) {
-			logger.error("parse bean to pretty json failed: " + e.getMessage());
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public static void sleep(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static boolean hasElement(Collection<?> collection) {
-		if (collection != null && !collection.isEmpty()) {
-			return true;
-		}
-		return false;
-	}
+    public static String prettyJson(Object bean) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(bean);
+        } catch (JsonProcessingException e) {
+            logger.error("parse bean to pretty json failed: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	public static boolean hasElement(Map<?, ?> map) {
-		if (map != null && !map.isEmpty()) {
-			return true;
-		}
-		return false;
-	}
+    public static boolean hasElement(Collection<?> collection) {
+        if (collection != null && !collection.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
 
-	public static String getTimeStamp() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
-		return sdf.format(new Date());
-	}
+    public static boolean hasElement(Map<?, ?> map) {
+        if (map != null && !map.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String getTimeStamp() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        return sdf.format(new Date());
+    }
 }
